38d9214db01cc14b6c9378f68504eb8820b532a8
vaibhav bisht <vaibhavbisht592@gmail.com>
2023-11-04 15:42:33 +0530

    added: display url after shortening

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -1,6 +1,8 @@
 'use client';
-import { ScissorsLineDashed } from 'lucide-react';
-import { Button, Input } from './ui';
+
+import { useRef } from 'react';
+import { CheckCircle2, Clipboard, ScissorsLineDashed } from 'lucide-react';
+import { Button, Input, useToast } from './ui';
 import { useMutation } from '@/common/hooks';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -12,15 +14,27 @@ import { useSession } from 'next-auth/react';
 import { useState } from 'react';
 import { AlertDialog } from './alert-dialog';
 import { useRouter } from 'next/navigation';
+import { Icons } from './icons';
+import Image from 'next/image';
+import Mountains from '../../public/images/mountains-1412683.svg';
 
 type UrlSubmitData = Omit<ShortendUrlData, 'userId'>;
+
+type ShortendUrlResponse = {
+    shortUrl: string;
+    longUrl: string;
+};
 
 const InputURL = () => {
     // Hooks.
     const [showDialog, setShowDialog] = useState(false);
 
+    const urlCopyRef = useRef<HTMLSpanElement>();
+    const { toast } = useToast();
+
     const router = useRouter();
-    const { data, isLoading, request } = useMutation();
+    const { data, isLoading, isSuccess, request } =
+        useMutation<ShortendUrlResponse>();
     const { data: userData, status: authStatus } = useSession();
 
     const {
@@ -46,6 +60,21 @@ const InputURL = () => {
         });
     };
 
+    const handleCopyUrl = async () => {
+        if (urlCopyRef && urlCopyRef.current) {
+            await navigator.clipboard.writeText(urlCopyRef.current.outerText);
+
+            toast({
+                title: (
+                    <span className="inline-flex">
+                        <CheckCircle2 className="stroke-green-400 mr-2 w-4" />
+                        Url Copied Successfully
+                    </span>
+                ),
+            });
+        }
+    };
+
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="flex w-full">
             <div className="flex w-full flex-col items-center justify-center gap-2">
@@ -53,10 +82,17 @@ const InputURL = () => {
                     <Input
                         type="text"
                         placeholder="Enter your long URL here.."
+                        className="my-5"
                         {...register('url')}
                     />
                     <Button type="submit" className="gap-2">
-                        <ScissorsLineDashed /> <span> Shorten </span>
+                        {isLoading ? (
+                            <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                        ) : (
+                            <>
+                                <ScissorsLineDashed /> <span> Shorten </span>
+                            </>
+                        )}
                     </Button>
                     <AlertDialog
                         title="Proceed to SingIn first?"
@@ -70,6 +106,32 @@ const InputURL = () => {
                         }}
                     />
                 </div>
+                {data && isSuccess && (
+                    <div className="flex justify-center items-center w-1/2">
+                        <div className="grid grid-cols-3 gap-y-3">
+                            <div className="font-bold"> Long Url </div>
+                            <div className="col-span-2 line-clamp-1 bg-white text-black px-2 p-0.5 rounded-md opacity-40">
+                                {data.longUrl}
+                            </div>
+                            <div className="font-bold"> Shorty Url</div>
+                            <div className="col-span-2">
+                                <span className="flex justify-center items-center gap-2 ">
+                                    <span
+                                        ref={urlCopyRef}
+                                        className="line-clamp-1 bg-white text-black px-2 p-0.5 rounded-md opacity-40"
+                                    >
+                                        {data.shortUrl}
+                                    </span>
+                                    <Clipboard
+                                        className="border-2 rounded-md bg-white opacity-40 text-black w-7 h-7 p-1 hover:stroke-gray-100 hover:bg-gray-600  hover:rounded hover:cursor-copy"
+                                        onClick={handleCopyUrl}
+                                    />
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                )}
+
                 <p className="errors text-sm">{errors?.url?.message}</p>
             </div>
         </form>
@@ -79,8 +141,20 @@ const InputURL = () => {
 export const HomePage = () => {
     return (
         <div className="h-full w-full flex flex-col items-center justify-center  ">
-            <h1> Shorty Url - The Fast and Reliable URL Shortener </h1>
-            <InputURL />
+            <Image
+                src={Mountains}
+                alt="homepage background"
+                objectFit="cover"
+                layout="fill"
+                className="-z-10 opacity-80"
+            />
+            <div className="backdrop-blur-sm p-3">
+                <h1 className="text-4xl mb-3">
+                    <strong> Shorty Url </strong> - The Fast and Reliable URL
+                    Shortener
+                </h1>
+                <InputURL />
+            </div>
         </div>
     );
 };
